7bdb60ec4ddb8e353d893ff9413b1629558b4e1b
Ahmed Said <ahmedelsaid@techytypes.com>
2021-08-03 00:30:05 +0200

    feat:Context Module Functions

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -9,9 +9,11 @@ import {dequal} from 'dequal'
 import * as userClient from '../user-client'
 import {useAuth} from '../auth-context'
 
+// user Context
 const UserContext = React.createContext()
 UserContext.displayName = 'UserContext'
 
+// user Reducer
 function userReducer(state, action) {
   switch (action.type) {
     case 'start update': {
@@ -73,9 +75,19 @@ function useUser() {
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
+// refactor update user function in order to separate the logic
+async function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates: updates})
+  try {
+    const updatedUser = await userClient.updateUser(user, updates)
+    dispatch({type: 'finish update', updatedUser})
+    return updatedUser
+  } catch (error) {
+    dispatch({type: 'fail update', error})
+    // return Promise.reject(error)
+    throw error
+  }
+}
 
 // export {UserProvider, useUser}
 
@@ -98,10 +110,8 @@ function UserSettings() {
   function handleSubmit(event) {
     event.preventDefault()
     // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
+    updateUser(userDispatch, user, formState).catch(error =>
+      console.log('error happened', error),
     )
   }
 
